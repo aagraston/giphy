0ea6b2ef36c591233940a16d372d4f439f0c5324
aagraston <aagraston@gmail.com>
2022-03-07 12:59:34 -0800

    html and styling added for search bar

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,10 @@ import '../scss/app.scss'
 const img = document.querySelector('img')
 
 fetch(
-  'https://api.giphy.com/v1/gifs/translate?api_key=ENgp3UU19tYVBEpsMUmNRxpvXOOMnfnW&s=cat',
+  'https://api.giphy.com/v1/gifs/translate?api_key=ENgp3UU19tYVBEpsMUmNRxpvXOOMnfnW&s=cats',
   { mode: 'cors' }
 )
-  .then(function (response) {
-    return response.json()
-  })
-  .then(function (response) {
+  .then((response) => response.json())
+  .then((response) => {
     img.src = response.data.images.original.url
   })
