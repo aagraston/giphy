d91cead1d049afdd74bfe14756fbb50f1394aeed
aagraston <aagraston@gmail.com>
2022-03-07 13:19:58 -0800

    search capabilities added

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,38 @@
 import '../scss/app.scss'
 
+// get elements from page
 const img = document.querySelector('img')
+const inputText = document.querySelector('#input-search-gif')
+const inputSubmit = document.querySelector('#input-submit-search')
 
-fetch(
-  'https://api.giphy.com/v1/gifs/translate?api_key=ENgp3UU19tYVBEpsMUmNRxpvXOOMnfnW&s=cats',
-  { mode: 'cors' }
-)
-  .then((response) => response.json())
-  .then((response) => {
-    img.src = response.data.images.original.url
-  })
+inputSubmit.addEventListener('click', performSearch)
+
+const queryStart =
+  'https://api.giphy.com/v1/gifs/translate?api_key=ENgp3UU19tYVBEpsMUmNRxpvXOOMnfnW&s='
+
+let queryFull = ''
+
+function performSearch(e) {
+  const searchValue = inputText.value
+
+  calculateQuery(searchValue)
+  searchImage()
+}
+
+function calculateQuery(searchString) {
+  const newString = queryStart + searchString
+  queryFull = newString
+  console.log(queryFull)
+}
+
+function searchImage() {
+  fetch(queryFull, { mode: 'cors' })
+    .then((response) => response.json())
+    .then((response) => {
+      displayImage(response.data.images.original.url)
+    })
+}
+
+function displayImage(imageUrl) {
+  img.src = imageUrl
+}
