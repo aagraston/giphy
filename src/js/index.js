b3e94c1ff33392eab7e12787b10147cd3291e103
aagraston <aagraston@gmail.com>
2022-03-07 10:41:10 -0800

    image acquired and added to webpage

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,10 @@ const img = document.querySelector('img')
 fetch(
   'https://api.giphy.com/v1/gifs/translate?api_key=ENgp3UU19tYVBEpsMUmNRxpvXOOMnfnW&s=cat',
   { mode: 'cors' }
-).then(function (response) {
-  console.log(response.json())
-})
+)
+  .then(function (response) {
+    return response.json()
+  })
+  .then(function (response) {
+    img.src = response.data.images.original.url
+  })
