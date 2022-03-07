ecc8448f6181c044309e2d50f76f15aad30c537c
aagraston <aagraston@gmail.com>
2022-03-07 10:30:57 -0800

    API accessed, console logged JSON

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,10 @@
 import '../scss/app.scss'
+
+const img = document.querySelector('img')
+
+fetch(
+  'https://api.giphy.com/v1/gifs/translate?api_key=ENgp3UU19tYVBEpsMUmNRxpvXOOMnfnW&s=cat',
+  { mode: 'cors' }
+).then(function (response) {
+  console.log(response.json())
+})
